13d66daf55b74828e05ee4075ee177ef59f77830
Suraj Pattade <surajpattade@gmail.com>
2023-08-19 01:13:50 +0530

    Implemented UI and backend of Auth

---
+++ api/index.js
@@ -0,0 +1,166 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const nodemailer = require("nodemailer");
+const crypto = require("crypto");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+
+const app = express();
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//mongo connection
+mongoose
+  .connect(
+    "mongodb+srv://surajpattade:surajpattade@cluster0.tm305us.mongodb.net/",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDb", err);
+  });
+
+const User = require("./models/user");
+const Order = require("./models/order");
+
+//---------------------------------------------------------------------------------------
+
+//SEND VERIFICATION MAIL
+
+const sendVerificationEmail = async (email, verificationToken) => {
+  // Create a Nodemailer transporter
+  const transporter = nodemailer.createTransport({
+    // Configure the email service or SMTP details here
+    service: "gmail",
+    auth: {
+      user: "solarflareodd@gmail.com",
+      pass: "xxx",
+    },
+  });
+
+  // Compose the email message
+  const mailOptions = {
+    from: "amazon.com",
+    to: email,
+    subject: "Email Verification",
+    text: `Please click the following link to verify your email: http://192.168.0.102:8000/verify/${verificationToken}`,
+  };
+
+  // Send the email
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log("Verification email sent successfully");
+  } catch (error) {
+    console.error("Error sending verification email:", error);
+  }
+};
+
+//REGISTER
+
+app.post("/register", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    // Check if the email is already registered
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      console.log("Email already registered:", email); // Debugging statement
+      return res.status(400).json({ message: "Email already registered" });
+    }
+
+    // Create a new user
+    const newUser = new User({ name, email, password });
+
+    // Generate and store the verification token
+    newUser.verificationToken = crypto.randomBytes(20).toString("hex");
+
+    // Save the user to the database
+    await newUser.save();
+
+    // Debugging statement to verify data
+    console.log("New User Registered:", newUser);
+
+    // Send verification email to the user
+    // Use your preferred email service or library to send the email
+    sendVerificationEmail(newUser.email, newUser.verificationToken);
+
+    res.status(201).json({
+      message:
+        "Registration successful. Please check your email for verification.",
+    });
+  } catch (error) {
+    console.log("Error during registration:", error); // Debugging statement
+    res.status(500).json({ message: "Registration failed" });
+  }
+});
+
+//VERIFY EMAIL
+
+app.get("/verify/:token", async (req, res) => {
+  try {
+    const token = req.params.token;
+
+    //Find the user witht the given verification token
+    const user = await User.findOne({ verificationToken: token });
+    if (!user) {
+      return res.status(404).json({ message: "Invalid verification token" });
+    }
+
+    //Mark the user as verified
+    user.verified = true;
+    user.verificationToken = undefined;
+
+    await user.save();
+
+    res.status(200).json({ message: "Email verified successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Email Verificatioion Failed" });
+  }
+});
+
+//LOGIN
+
+const generateSecretKey = () => {
+  const secretKey = crypto.randomBytes(32).toString("hex");
+  return secretKey;
+};
+const secretKey = generateSecretKey();
+
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    //check if the user exists
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    //check if the password is correct
+    if (user.password !== password) {
+      return res.status(401).json({ message: "Invalid password" });
+    }
+
+    //generate a token
+    const token = jwt.sign({ userId: user._id }, secretKey);
+
+    res.status(200).json({ token });
+  } catch (error) {
+    res.status(500).json({ message: "Login Failed" });
+  }
+});
+
+//server listen
+const PORT = 8000;
+
+app.listen(PORT, "192.168.0.102", (req, res) => {
+  console.log(`Server running on PORT ${PORT}`);
+});
---
+++ screens/LoginScreen.js
@@ -0,0 +1,186 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  KeyboardAvoidingView,
+  TextInput,
+  Pressable,
+} from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { MaterialIcons } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const LoginScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigation = useNavigation();
+
+  const handleLogin = async () => {
+    const userData = { email, password };
+    try {
+      const res = await axios.post("http://192.168.0.102:8000/login", userData);
+      const token = res.data.token;
+      AsyncStorage.setItem("authToken", token);
+      navigation.replace("Home");
+    } catch (error) {
+      Alert.alert("Login Error", `${error.message}`);
+      console.log("Login failed", error);
+    }
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      {/* logo */}
+      <View>
+        <Image
+          style={styles.logo}
+          source={{
+            uri: "https://assets.stickpng.com/thumbs/6160562276000b00045a7d97.png",
+          }}
+        />
+      </View>
+
+      {/* Screen */}
+      <KeyboardAvoidingView>
+        <View style={styles.logoView}>
+          <Text style={styles.logoText}>Login in to your account</Text>
+        </View>
+
+        {/* Inputs */}
+        <View style={styles.inputsView}>
+          <View style={styles.inputEmail}>
+            <MaterialIcons
+              name="email"
+              size={24}
+              color="gray"
+              style={styles.inputEmailIcon}
+            />
+            <TextInput
+              placeholder="Enter your email"
+              value={email}
+              style={styles.inputEmailInput}
+              onChangeText={(text) => setEmail(text)}
+            />
+          </View>
+          <View style={styles.inputEmail}>
+            <AntDesign
+              name="lock"
+              size={24}
+              color="gray"
+              style={styles.inputEmailIcon}
+            />
+            <TextInput
+              placeholder="Enter your password"
+              value={password}
+              style={styles.inputEmailInput}
+              secureTextEntry={true}
+              onChangeText={(text) => setPassword(text)}
+            />
+          </View>
+        </View>
+
+        <View style={styles.extraView}>
+          <Text>Keep me logged in</Text>
+          <Text style={styles.forgetPassword}>Forget password ?</Text>
+        </View>
+
+        {/* Login Button */}
+        <View style={styles.btnView}>
+          <Pressable style={styles.pressable} onPress={handleLogin}>
+            <Text style={styles.login}>Login</Text>
+          </Pressable>
+        </View>
+
+        <Pressable
+          style={styles.redirectView}
+          onPress={() => navigation.navigate("Register")}
+        >
+          <Text style={styles.redirectMsg}>Don't have an account? Sign Up</Text>
+        </Pressable>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
+  );
+};
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    alignItems: "center",
+  },
+  logo: {
+    width: 150,
+    height: 100,
+  },
+  logoView: {
+    alignItems: "center",
+  },
+  logoText: {
+    fontSize: 17,
+    fontWeight: "bold",
+    marginTop: 12,
+    color: "#041e42",
+  },
+  inputsView: {
+    marginTop: 70,
+  },
+  inputEmail: {
+    flexDirection: "row",
+    gap: 5,
+    backgroundColor: "#d0d0d0",
+    alignItems: "center",
+    marginTop: 30,
+    paddingVertical: 5,
+    borderRadius: 5,
+  },
+  inputEmailIcon: {
+    marginLeft: 8,
+  },
+  inputEmailInput: {
+    color: "gray",
+    width: 300,
+    marginVertical: 10,
+    fontSize: 16,
+  },
+  extraView: {
+    marginTop: 12,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  forgetPassword: {
+    color: "#007fff",
+    fontWeight: "500",
+  },
+  btnView: {
+    marginTop: 60,
+    alignItems: "center",
+  },
+  pressable: {
+    width: 200,
+    backgroundColor: "#febe10",
+    padding: 15,
+    borderRadius: 6,
+  },
+  login: {
+    textAlign: "center",
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  redirectView: {
+    marginTop: 15,
+  },
+  redirectMsg: {
+    color: "gray",
+    textAlign: "center",
+    fontSize: 16,
+  },
+});
